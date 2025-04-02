e85b917507de5292643a542a2d46dcb27b3ed721
Thiago <1thiago1henrique@gmail.com>
2025-04-02 18:36:41 -0300

    Inicio da aula sobre dicionários

---
+++ 01_listas_tuplas_dicionarios/main.py
@@ -0,0 +1,20 @@
+x = 5
+#print("teste")
+y = [1,2,3,4,5]
+
+contador = 0
+
+for value in y:
+    print(value)
+
+
+filmes = ("star wars", "severance");
+for filme in filmes:
+    print(filme);
+
+print("--------------")
+
+SEMANA = ("segunda", "terça", "QUARTA", "quinta", "sexta", "sábado", "domingo")
+
+for dia in SEMANA:
+    print(dia)
